0374d81a776695b0db7351d6c2fb2e3238859c31
sp <sam.pack.sp@gmail.com>
2020-09-20 18:18:09 +0930

    dynamic buttons working

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 $(document).ready(function(){
 
  
-    
+    var city ="";
 
     $("#searchBtn").on('click', function(){
 
         var city = $("#cityInput").val();
-        $("#buttonDiv").append("<button>"+city+"</button>");
-        
+        console.log(city);
+        var addButton = document.createElement('button');
+        addButton.setAttribute('class', 'savehistory btn btn btn-secondary m-1 custom');
+        addButton.textContent = city ;
+        $(".buttonDiv").append(addButton);
+        //$(".buttonDiv").append("<button>"+city+"</button><br>");
+        startQuery();
+    });
+
+    $(".buttonDiv").on('click', "button.savehistory", function(){
+        var city = $(this).text();
+        console.log(city);
+        startQuery();
+    })
+
+    function startQuery(){
+
+
         var queryurl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=4c47461c514bba9e1bbc913bffac4116";
         console.log(city);
     
@@ -23,10 +39,6 @@ $(document).ready(function(){
                 console.log(response);
                 });
 
-  
+    };
         
-        });
-
-
-        
-    });
+}); 
