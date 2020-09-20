1af6fa9afb80513ca553da92b5d45ae687c90086
sp <sam.pack.sp@gmail.com>
2020-09-20 16:22:22 +0930

    js file search history buttons adding, ajax query not working cors error

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
-$(document).ready(){
+$(document).ready(function(){
 
-
-
-
-
-
+ 
     
-}
+
+    $("#searchBtn").on('click', function(){
+
+        var city = $("#cityInput").val();
+        $("#buttonDiv").append("<button>"+city+"</button>");
+        
+        var queryurl = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=4c47461c514bba9e1bbc913bffac4116";
+        console.log(city);
+    
+        $.ajax({
+            url: queryurl,
+            type: "GET",
+        
+            //success: function(){},
+            //error: function(){ 
+               // alert("We are unable to process your search right now. Try again later.");},
+
+            }).then(function(response) {
+                console.log(response);
+                });
+
+  
+        
+        });
+
+
+        
+    });
